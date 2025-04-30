3121ddbc06bd8b0b4bf8f09bd2e24f950b21f9b6
Aarthi3730 <aarthikodipalli@gmail.com>
2025-04-30 21:33:37 +0530

    Practiced java programming

---
+++ src/Main/java/MyFirstCode.java
@@ -0,0 +1,7 @@
+package Main.java;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("This is my first code");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args){
+        int age =20;
+        System.out.println(age);
+    }
+}
