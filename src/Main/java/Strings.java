4ca110fff90a7b07920b9c02d3f8c77371780c25
Aarthi3730 <aarthikodipalli@gmail.com>
2025-05-02 19:05:46 +0530

    Updated the code

--- src/Main/java/Strings.java
+++ src/Main/java/Strings.java
@@ -1,6 +1,14 @@
+import java.sql.SQLOutput;
+
 class Srings{
     public static void main(String[] args){
         String message="Hello World"+"!!";
         System.out.println(message);
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
     }
 }
