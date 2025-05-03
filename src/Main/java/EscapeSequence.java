8b9cc38e1010dc39ae1a74ed4363f720301e78df
Aarthi3730 <aarthikodipalli@gmail.com>
2025-05-04 00:30:02 +0530

    Create EscapeSequence.java

---
+++ src/Main/java/EscapeSequence.java
@@ -0,0 +1,10 @@
+package Main.java;
+
+class EscapeSequence{
+    public static void main(String[] args){
+         String message="Hello\"Mosh";
+        System.out.println(message);
+        String S="(:\\Windows\\...";
+        System.out.println(S);
+    }
+}
