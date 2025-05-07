f6bd148da3011131b18b3785bafb91e18be211da
Aarthi3730 <aarthikodipalli@gmail.com>
2025-05-07 20:48:34 +0530

    Create PizzaSplit.java

---
+++ src/Main/java/PizzaSplit.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner Scanner=new Scanner(System.in);
+        int number=Scanner.nextInt();
+        if(number%2==0){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("2");
+        }
+    }
+}
