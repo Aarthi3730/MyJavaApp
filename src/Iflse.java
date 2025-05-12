992bdd720d45959472396019752933de0ebdc142
Aarthi3730 <aarthikodipalli@gmail.com>
2025-05-12 22:52:51 +0530

    Create Iflse.java

---
+++ src/Iflse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class NumberCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // If-else logic
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
