9a7466f39323bbfd6d2377b08415cb0bbbea340d
Aarthi3730 <aarthikodipalli@gmail.com>
2025-05-21 23:21:17 +0530

    Create ArrayExample.java

---
+++ src/Main/java/ArrayExample.java
@@ -0,0 +1,22 @@
+ class ArrayExample {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the length of the array
+        System.out.println("Length of array: " + numbers.length);
+
+        // Print all elements using a for loop
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+
+        // Find the sum of all elements
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
